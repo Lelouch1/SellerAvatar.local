d25807232b8fc6870447acd95479b40475bb94d8
Koroteev Dmitry <desing94root@gmail.com>
2021-04-26 11:40:41 +0300

    init app

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+    'DB_HOST'=>'127.0.0.1',
+    'DB_USERNAME'=>'root',
+    'DB_PASSWORD'=>'',
+    'DB_DATABASE'=>''
+);
+?>
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const dir = {
+    src: './',
+    dest : './assets/'
+}
+
+const js = {
+    src: dir.src + 'js/*.js',
+    dest: dir.dest + 'js',
+}
+
+const css = {
+    src : dir.src + 'css/*.scss',
+    dest: dir.dest + 'css',
+}
+
+gulp.task('build-js', async () => {
+    return gulp.src(js.src)
+        .pipe(babel({presets: [['@babel/preset-env',]]}))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(js.dest))
+});
+gulp.task('build-css', async () => {
+    return gulp.src(css.src)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 9 version', '> 0.5%'],
+            cascade: false
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(css.dest))
+})
+
+gulp.task('default', gulp.series('build-js', 'build-css'));
+gulp.task('watch', function() {
+    gulp.watch(dir.src + 'js/*.js', gulp.series('build-js'));
+    gulp.watch(dir.src + 'css/*.scss', gulp.series('build-css'));
+});
